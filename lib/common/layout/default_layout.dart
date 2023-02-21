c6a6274047242988473a6330207ef061ec56215b
nomad2569 <nomad2569@naver.com>
2023-02-21 20:37:15 +0900

    feat: add `tab_bar` `bottom_nav_bar`

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -5,13 +5,41 @@ import 'package:flutter/src/widgets/placeholder.dart';
 class DefaultLayout extends StatelessWidget {
   final Widget child;
   final Color? bgColor;
-  const DefaultLayout({super.key, required this.child, this.bgColor});
+  final String? title;
+  final BottomNavigationBar? bottomNavigationBar;
+  const DefaultLayout({
+    super.key,
+    required this.child,
+    this.bgColor,
+    this.title,
+    this.bottomNavigationBar,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: bgColor ?? Colors.white,
+      appBar: renderAppBar(),
       body: child,
+      bottomNavigationBar: bottomNavigationBar,
     );
+  }
+
+  AppBar? renderAppBar() {
+    // AppBar 사용하지 않을 때
+    if (title == null) {
+      return null;
+    } else {
+      return AppBar(
+        backgroundColor: Colors.white,
+        // AppBar 가 앞으로 튀어나오는 정도
+        elevation: 0,
+        title: Text(title!,
+            style: TextStyle(
+              fontSize: 16.0,
+            )),
+        foregroundColor: Colors.black,
+      );
+    }
   }
 }
--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -1,15 +1,95 @@
+import 'package:codefactory_intermediate/common/const/colors.dart';
+import 'package:codefactory_intermediate/common/layout/default_layout.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class RootTab extends StatelessWidget {
+class RootTab extends StatefulWidget {
   const RootTab({super.key});
 
   @override
+  State<RootTab> createState() => _RootTabState();
+}
+
+class _RootTabState extends State<RootTab> with SingleTickerProviderStateMixin {
+  late TabController controller;
+
+  int currIndex = 0;
+  @override
+  void initState() {
+    super.initState();
+
+    // TabController 사용시 `SingleTickerProviderStateMixin` 을 무조건 사용해야함
+    // 애니메이션 관련된 widget 은 이러는 경우가 있음.
+    controller = TabController(length: 4, vsync: this);
+
+    controller.addListener(tabListener);
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(tabListener);
+
+    super.dispose();
+  }
+
+  void tabListener() {
+    setState(() {
+      currIndex = controller.index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Root Tab!!'),
+    return DefaultLayout(
+      title: '홈 화면',
+      child: TabBarView(
+        // * TabBar 가로 스크롤 조정 가능
+        physics: NeverScrollableScrollPhysics(),
+        controller: controller,
+        children: [
+          Center(child: Text('홈')),
+          Center(child: Text('음식')),
+          Center(child: Text('주문')),
+          Center(child: Text('프로필')),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        // 선택된 아이템 색상
+        selectedItemColor: PRIMARY_COLOR,
+        unselectedItemColor: BODY_TEXT_COLOR,
+        selectedFontSize: 10,
+        unselectedFontSize: 10,
+        // 애니메이션하려면 `shifting`
+        type: BottomNavigationBarType.fixed,
+        // `setState` 로 현재 tabIndex 를 지정한다.
+        onTap: (int index) {
+          controller.animateTo(index);
+          //// currentIndex setState
+          // setState(() {
+          //   currIndex = index;
+          // });
+        },
+        // 현재 Tab 의 index 정보 : `currentIndex`
+        currentIndex: currIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fastfood_outlined),
+            label: '음식',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.receipt_long_outlined),
+            label: '주문',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.percent_outlined),
+            label: '프로필',
+          ),
+        ],
       ),
     );
   }
